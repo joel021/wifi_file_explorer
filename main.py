533685481251289204893b9e55b7f34dc0f1c928
Joel Pires <joelpires70@gmail.com>
2023-06-01 20:46:40 -0300

    refact: clean code and add requirements

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-#!/usr/bin/env python
-#
-# deepzoom_multiserver - Example web application for viewing multiple slides
-#
-# Copyright (c) 2010-2015 Carnegie Mellon University
-#
-# This library is free software; you can redistribute it and/or modify it
-# under the terms of version 2.1 of the GNU Lesser General Public License
-# as published by the Free Software Foundation.
-#
-# This library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-# License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this library; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import os
-import json
-from flask import Flask, abort, make_response, render_template, url_for, request, redirect, send_file
+from flask import Flask,  make_response, render_template, request, send_file
 import webbrowser
 import subprocess
 import sys
-import glob
 
 class _Directory:
     def __init__(self, basedir, relpath=''):
